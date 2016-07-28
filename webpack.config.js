4f4069773b2cc4192a0473d84e34a7ca2a5105ab
Travis Shields <travis.shields@gmail.com>
2016-07-28 14:41:39 -0400

    Add alias for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,19 @@ module.exports = {
   resolve: {
   root: __dirname,
   alias: {
+    AddTask: 'app/components/AddTask.js',
+    EditTask: 'app/components/EditTask.js',
+    Home: 'app/components/Home.js',
+    keys: 'app/components/keys.js',
+    ListTask: 'app/components/ListTask.js',
+    Main: 'app/components/Main.js',
+    AddContainer: 'app/containers/AddContainer.js',
+    DeleteTemp: 'app/containers/DeleteTemp.js',
+    EditTaskContainer: 'app/containers/EditTaskContainer.js',
+    ListTaskContainer: 'app/containers/ListTaskContainer.js',
+    routes: 'app/config/routes.js',
+    HomeStyles: 'app/styles/HomeStyles.js',
+    ajazHelpers: 'app/utils/ajazHelpers.js',
   },
   extensions: ['', '.js', '.jsx']
 },
